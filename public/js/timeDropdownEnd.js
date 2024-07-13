b21a094aca28d30bc656d8d90077564296b12ca0
YangyuYUI <yangyubroadcast@gmail.com>
2024-07-13 17:43:52 +0800

    addCourseFixed

--- public/js/timeDropdownEnd.js
+++ public/js/timeDropdownEnd.js
@@ -1,24 +1,38 @@
 import { getCourses } from "../api.js";
 import { createTable } from "./courseTable.js";
 const startTime = 9; // 開始時間 9 點
-const endTime = 18; // 結束時間 6 點
+const endTime = 19; // 結束時間 6 點
+var timeNum = 0;
+//總共有19個時間選項
 
 document.addEventListener("DOMContentLoaded", function () {
   // 更新下拉菜单内容
   function updateTimeDropdown() {
     var dropdownMenu = document.querySelector("#timeDropdownEnd");
     dropdownMenu.innerHTML = ""; // 清空原有内容
-    
+    var numCount = 0;
+
     for (let hour = startTime; hour < endTime; hour++) {
       for (let minute = 0; minute < 60; minute += 30) {
           const time = `${hour.toString().padStart(2, "0")}:${minute
               .toString()
               .padStart(2, "0")}`;
   
+
+
+
           var dropdownItem = document.createElement("li");
           dropdownItem.classList.add("dropdown-item");
+          
+          
+          dropdownItem.value = numCount;
+          
+          numCount++;
+
           dropdownItem.innerHTML = time;
           dropdownMenu.appendChild(dropdownItem);
+          
+          
       }
   }}
   
@@ -32,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", function (event) {
       var selectedDate = event.target.textContent;
 
-      alert(event.target.innerHTML);
+      
       //document.querySelector('#dateDropdown').textContent = selectedDate;
 
       //選擇後操作~~~~~~~~~~~~~~~~~~
@@ -46,7 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       var selection = document.getElementById("timeDropdownEnd");
       function select() {
-        alert(selection.options[selection.selectedIndex].value);
+        
       }
     });
 
--- public/js/timeDropdownStart.js
+++ public/js/timeDropdownStart.js
@@ -1,13 +1,14 @@
 import { getCourses } from "../api.js";
 import { createTable } from "./courseTable.js";
 const startTime = 9; // 開始時間 9 點
-const endTime = 18; // 結束時間 6 點
+const endTime = 19; // 結束時間 6 點
 
 document.addEventListener("DOMContentLoaded", function () {
   // 更新下拉菜单内容
   function updateTimeDropdown() {
     var dropdownMenu = document.querySelector("#timeDropdownStart");
     dropdownMenu.innerHTML = ""; // 清空原有内容
+    var numCount = 0;
     
     for (let hour = startTime; hour < endTime; hour++) {
       for (let minute = 0; minute < 60; minute += 30) {
@@ -17,6 +18,10 @@ document.addEventListener("DOMContentLoaded", function () {
   
           var dropdownItem = document.createElement("li");
           dropdownItem.classList.add("dropdown-item");
+
+          dropdownItem.value = numCount;
+          
+          numCount++;
           dropdownItem.innerHTML = time;
           dropdownMenu.appendChild(dropdownItem);
       }
@@ -32,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", function (event) {
       var selectedDate = event.target.textContent;
 
-      alert(event.target.innerHTML);
+      
       //document.querySelector('#dateDropdown').textContent = selectedDate;
 
       //選擇後操作~~~~~~~~~~~~~~~~~~
@@ -46,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       var selection = document.getElementById("timeDropdownStart");
       function select() {
-        alert(selection.options[selection.selectedIndex].value);
+        
       }
     });
 
